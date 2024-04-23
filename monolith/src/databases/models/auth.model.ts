4b66ffb631adf43c4650d3286be6eecfa35dbe9d
Mee Reak <138286828+MeeReak@users.noreply.github.com>
2024-04-23 08:22:06 +0700

    add new model

--- monolith/src/databases/models/auth.model.ts
+++ monolith/src/databases/models/auth.model.ts
@@ -1,17 +1,17 @@
-import mongoose , {Document, Schema} from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 
-export interface IAuth extends Document{
-    email       : string,
-    password    : string,
-    googleId    : string,
-    isVerify    : boolean
+export interface IAuth extends Document {
+  email: string;
+  password: string;
+  googleId: string;
+  isVerify: boolean;
 }
 
-const authSchema:Schema = new Schema({
-    email : { type: String, required: true, unique: true },
-    password : {type: String , require: true},
-    googleId : {type: String , unique: true},
-    isVerify : {type:Boolean , default: false},
-})
+const authSchema: Schema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, require: true },
+  googleId: { type: String, unique: true },
+  isVerify: { type: Boolean, default: false },
+});
 
 export const Auth = mongoose.model<IAuth>("Auth", authSchema);
